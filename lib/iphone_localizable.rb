db32af4c0598572ea5a050bcad237de701ec3aac
Tomislav Car <tomislav@infinum.hr>
2011-07-29 17:31:50 +0200

    Stare promjene za win7 i wrt

--- lib/iphone_localizable.rb
+++ lib/iphone_localizable.rb
@@ -3,7 +3,7 @@ class IphoneLocalizable
     puts "Loading #{filename}"
     @filename = filename
     @basename = File.basename(@filename)
-    @locale = options[:locale] || @basename.match(/(.*)_Localizable.strings/)[1]
+    @locale = options[:locale] || File.basename(File.dirname(@filename)).gsub(".lproj", "")
 #    @contents = File.open(filename, 'r')
     @data = []
     read
@@ -14,9 +14,9 @@ class IphoneLocalizable
     x = nil
     File.open(@filename, 'r') {|f| x = f.readlines }
     x.each do |line|
-      puts line
+
       line = self.class.remove_comments(line)
-puts line
+
       if line.present?
         @data << self.class.extract_data_from_line(line)
         puts self.class.extract_data_from_line(line).to_yaml
@@ -32,18 +32,26 @@ puts line
   def self.extract_data_from_line(line)
     raw = line.split('"').map(&:strip)
 
-    original = convert_variables(raw[1])
-    value = convert_variables(raw[3])
+    original = format_value(convert_variables(raw[1]))
+    value = format_value(convert_variables(raw[3]))
 
     data = {
-      :key => original.gsub(" ", "_").gsub(/[^a-zA-Z0-9_]/, '').to_s, :value => value, :original => original
+      :key => original.gsub("+", "Plus").gsub("-", "Minus").gsub(" ", "_").gsub(/[^a-zA-Z0-9_]/, '').to_s, :value => value, :original => original
     }
   
     data
   end
   
   def self.convert_variables(string)
-    string.gsub("%@", "%s")
+    string.gsub("%@", "%s").gsub("%u", "%d")
+  end
+  
+  def self.format_value(string)
+    return string # for wrt
+    
+    x = "\\"
+    bla = "asdasdasdasdasd"
+    string.gsub("'", x + bla + "'").gsub(bla, "")
   end
   
   def to_android(options = {})
@@ -64,6 +72,47 @@ puts line
     decoder.decode(builder.target!)
   end
   
+  def to_win7(options = {})
+    value_key = options[:original] ? :original : :value      
+
+    $KCODE = 'UTF8'
+    builder = Builder::XmlMarkup.new(:indent=>2)
+    decoder = HTMLEntities.new
+    #builder.instruct! :xml, :version=>"1.0", :encoding=>"UTF-8"    
+    
+    xml = builder.mmaakknnii do |resources|
+      @data.each do |translation|
+        resources.data(:name => translation[:key]) do |bla|
+          bla.value(translation[value_key])
+        end
+      end
+    end    
+    
+    result = decoder.decode(builder.target!)
+    result.gsub!('<mmaakknnii>', '').gsub!('</mmaakknnii>', '')
+    
+
+    f = File.read('OlxResources.resx')
+    f.gsub('<!--- insert here --->', result)
+#    puts xml
+#    decoder.decode(builder.target!)
+  end  
+  
+  def to_wrt(options = {})
+    value_key = options[:original] ? :original : :value   
+        
+    str = ''
+    str << "var olx;"
+    str <<  "if (!olx || !olx.strings) {"
+    str << 'alert("olx module has not been loaded");'
+    str << '}'
+    @data.each do |translation|
+      str << "olx.strings[\"#{translation[:original]}\"] = \"#{translation[value_key]}\";\n"
+    end
+    
+    str
+  end
+  
   def save_as_android!(options = {})
     FileUtils.mkdir_p android_directory
     File.open(File.join(android_directory, 'strings.xml'), 'w+') do |f|
@@ -71,7 +120,29 @@ puts line
     end
   end
   
+  def save_as_win7!(options = {})
+    FileUtils.mkdir_p win7_directory
+    File.open(File.join(win7_directory, 'OlxResources.resx'), 'w+') do |f|
+      f.puts to_win7(options)
+    end    
+  end
+  
+  def save_as_wrt!(options = {})
+    FileUtils.mkdir_p wrt_directory
+    File.open(File.join(wrt_directory, "localizedTextStrings.js"), 'w+') do |f|
+      f.puts to_wrt(options)
+    end    
+  end  
+  
   def android_directory
     "android/values-#{@locale}"
+  end
+  
+  def win7_directory
+    "win7/#{@locale}"
+  end
+  
+  def wrt_directory
+    "wrt/#{@locale}.lproj"
   end
 end
--- main.rb
+++ main.rb
@@ -10,13 +10,15 @@ $KCODE = 'UTF8'
 
 gem 'builder', '2.1.2'
 
-input_files = Dir['iphone/*.strings'].sort
+input_files = Dir['iphone/**/Localizable.strings'].sort
 
 iphone = IphoneLocalizable.new(input_files.first, :locale => :en)
 iphone.save_as_android!(:original => true)
 
 input_files.each do |file|
   iphone = IphoneLocalizable.new(file)
-  iphone.save_as_android!
+ # iphone.save_as_android!
+ iphone.save_as_wrt!
+#iphone.save_as_win7!
 end
 
